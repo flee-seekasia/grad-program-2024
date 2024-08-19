6bd9bb3b7ed19d54505f4af3bf1076147fbf6bef
fabiolee <flee@seekasia.com>
2024-08-19 15:26:22 +0800

    Create a simple view with general UI component

--- app/src/main/java/my/com/jobstreet/gradprogram/MainActivity.kt
+++ app/src/main/java/my/com/jobstreet/gradprogram/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import my.com.jobstreet.gradprogram.ui.theme.Gradprogram2024Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Gradprogram2024Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Group(paddingValues = innerPadding)
                 }
             }
         }
@@ -31,17 +38,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+private fun Group(paddingValues: PaddingValues = PaddingValues()) {
+    Column(
+        modifier = Modifier
+            .padding(paddingValues = paddingValues)
+            .padding(horizontal = 16.dp)
+    ) {
+        GroupTitle()
+        GroupDescription()
+    }
+}
+
+@Composable
+private fun GroupTitle() {
+    Row {
+        Column(modifier = Modifier.weight(weight = 1f)) {
+            Text(text = "Local Group 1", style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.height(height = 8.dp))
+            Text(text = "1k followers", style = MaterialTheme.typography.bodyLarge)
+        }
+        Spacer(modifier = Modifier.width(width = 8.dp))
+        Button(onClick = { }) {
+            Text(text = "Follow", style = MaterialTheme.typography.labelLarge)
+        }
+    }
+}
+
+@Composable
+private fun GroupDescription() {
+    Spacer(modifier = Modifier.height(height = 16.dp))
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Description for local group to help candidates understand how this could help. MAX 2 lines.",
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 2,
+        style = MaterialTheme.typography.bodyLarge
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewGroup() {
     Gradprogram2024Theme {
-        Greeting("Android")
+        Group()
     }
 }
