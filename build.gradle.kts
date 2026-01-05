46de04ade2a152f30806c89d2c983fce27704386
Fabio Lee <flee@seekasia.com>
2026-01-05 12:18:20 +0800

    MAA-5612 Add "com.buildkite.test-collector-android:unit-test-collector-plugin" for testing
    (cherry picked from commit 128f86d0826cd2cdce916dcca68d4fba2a767a0c)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath("com.buildkite.test-collector-android:unit-test-collector-plugin:0.1.0")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.buildkite.test-collector-android.unit-test-collector-plugin") version "0.1.0" apply false
 }
